311d9d4ac03f2ef1409cddbe6625bde9c54cda04
Alex Perez <alejandro.gperez745@gmail.com>
2024-01-12 14:51:48 -0600

    changed telescope ff keybind

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,8 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function ()
     local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<C-Space>', builtin.find_files, {})
+    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+    --vim.keymap.set('n', '<C-Space>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
   end
@@ -24,4 +25,3 @@ return {
     end
   },
 }
-
