2be467bca94da3e26e63554f740728ec16e07b4f
Alex Perez <alejandro.gperez745@gmail.com>
2024-01-22 16:08:23 -0600

    added tailwind colorization

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp"
-  },
-  {
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets"
+			"rafamadriz/friendly-snippets",
 		},
 	},
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
 				snippet = {
@@ -25,6 +25,9 @@ return {
 				window = {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
+				},
+				formatting = {
+					format = require("tailwindcss-colorizer-cmp").formatter,
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -39,6 +42,25 @@ return {
 				}, {
 					{ name = "buffer" },
 				}),
+			})
+		end,
+	},
+	{
+		"roobert/tailwindcss-colorizer-cmp.nvim",
+		-- optionally, override the default options:
+		config = function()
+			require("tailwindcss-colorizer-cmp").setup({
+				color_square_width = 2,
+			})
+		end,
+	},
+	{
+		"NvChad/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({
+				user_default_options = {
+					tailwind = true,
+				},
 			})
 		end,
 	},
