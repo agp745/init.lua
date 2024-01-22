2260cd764d057fcf2ea15656c7ecc166474fd143
Alex Perez <alejandro.gperez745@gmail.com>
2024-01-22 12:07:36 -0600

    C/C++ lsp support)

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,36 +1,38 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    null_ls.setup({
-      -- format on save
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format()
-              -- vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
+		null_ls.setup({
+			-- format on save
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format()
+							-- vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
 
-      sources = {
-        -- Lua
-        null_ls.builtins.formatting.stylua,
-        -- Javascript
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.diagnostics.eslint_d,
-        -- Go
-        null_ls.builtins.formatting.gofumpt,
-      },
-    })
+			sources = {
+				-- Lua
+				null_ls.builtins.formatting.stylua,
+				-- Javascript
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.diagnostics.eslint_d,
+				-- Go
+				null_ls.builtins.formatting.gofumpt,
+				-- C / C++
+				null_ls.builtins.formatting.clang_format,
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
